27e7c29983a26f232e3ee78878708d1d6798b206
AmirFone <ahossain20@fordham.edu>
2022-08-27 09:48:06 -0400

    HE

---
+++ AWS project /app/loisAI _fastapi.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from loisAI  import generate_branding_snippet, generate_keywords
+from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+handler = Mangum(app)
+MAX_INPUT_LENGTH = 32
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/generate_keywords")
+async def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": None, "keywords": keywords}
+
+
+
+def validate_input_length(prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Input length is too long. Must be under {MAX_INPUT_LENGTH} characters.",
+        )
